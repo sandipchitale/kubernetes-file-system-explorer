5d11446b462ba7bd7ad4f31d5dd33325b8c7fff2
SANDIP CHITALE <sandipchitale@gmail.com>
2019-11-02 02:50:43 +0100

    Show containers.

--- README.md
+++ README.md
@@ -46,3 +46,7 @@ Add screenshot and description.
 ### 1.0.2
 
 Add command `k8s.pod.container.file.view` (View file) - Show the content of the file in editor.
+
+### 1.0.3
+
+Show container.
--- src/extension.ts
+++ src/extension.ts
@@ -108,6 +108,65 @@ class FileSystemNodeContributor {
         return [];
     }
 }
+class ContainerNode implements k8s.ClusterExplorerV1.Node {
+    private kubectl: k8s.KubectlV1;
+    private podName: string;
+    namespace: string;
+    name: string;
+    private image: string;
+
+    constructor(kubectl:  k8s.KubectlV1, podName: string, namespace: string, name: string, image: string) {
+        this.kubectl = kubectl;
+        this.podName = podName;
+        this.namespace = namespace;
+        this.name = name;
+        this.image = image;
+    }
+
+    async getChildren(): Promise<k8s.ClusterExplorerV1.Node[]> {
+        return [];
+    }
+
+    getTreeItem(): vscode.TreeItem {
+        const treeItem = new vscode.TreeItem(`${this.name} ( ${this.image } )`, vscode.TreeItemCollapsibleState.None);
+        treeItem.tooltip = `${this.name} ( ${this.image } )`;
+        treeItem.iconPath = vscode.ThemeIcon.File;
+        return treeItem;
+    }
+}
+
+class ContainerNodeContributor {
+
+    private kubectl: k8s.KubectlV1;
+
+    constructor(kubectl:  k8s.KubectlV1) {
+        this.kubectl = kubectl;
+    }
+
+    contributesChildren(parent: k8s.ClusterExplorerV1.ClusterExplorerNode | undefined): boolean {
+        return parent && parent.nodeType === 'resource' && parent.resourceKind.manifestKind === 'Pod';
+    }
+
+    async getChildren(parent: k8s.ClusterExplorerV1.ClusterExplorerNode | undefined): Promise<k8s.ClusterExplorerV1.Node[]> {
+        const containers = [];
+        if (parent && parent.nodeType === 'resource' && parent.resourceKind.manifestKind === 'Pod') {
+            const explorer = await k8s.extension.clusterExplorer.v1;
+            if (explorer.available) {
+                const kubectl = await k8s.extension.kubectl.v1;
+                if (kubectl.available) {
+                    const podDetails = await kubectl.api.invokeCommand(`get pods ${parent.name} -o json`);
+                    if (podDetails && podDetails.stdout) {
+                        const podDetailsAsJson = JSON.parse(podDetails.stdout);
+                        podDetailsAsJson.spec.containers.forEach((container) => {
+                            containers.push(new ContainerNode(this.kubectl, parent.namespace, parent.name, container.name, container.image));
+                        });
+                    }
+                }
+            }
+        }
+        return containers;
+    }
+}
 
 export async function activate(context: vscode.ExtensionContext) {
     const explorer = await k8s.extension.clusterExplorer.v1;
@@ -122,13 +181,14 @@ export async function activate(context: vscode.ExtensionContext) {
         return;
     }
 
+    explorer.api.registerNodeContributor(new ContainerNodeContributor(kubectl.api));
     explorer.api.registerNodeContributor(new FileSystemNodeContributor(kubectl.api));
-
     const disposable = vscode.commands.registerCommand('k8s.pod.container.file.view', viewFile);
     context.subscriptions.push(disposable);
 }
 
 async function viewFile(target?: any) {
+
     if (target && target.nodeType === 'extension') {
         if (target.impl instanceof FileNode) {
             (target.impl as FileNode).viewFile();
